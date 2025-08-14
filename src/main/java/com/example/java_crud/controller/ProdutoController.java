1e776e36c0cccbbb9945d353bfd0601e240864fb
gherardijoao <gherardijoao@gmail.com>
2025-08-14 20:09:05 -0300

    post endpoint

---
+++ src/main/java/com/example/java_crud/controller/ProdutoController.java
@@ -0,0 +1,20 @@
+package com.example.java_crud.controller;
+import com.example.java_crud.model.Produto;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping ("produtos")
+public class ProdutoController {
+
+    @PostMapping
+    public Produto salvar(@RequestBody Produto produto) {
+        System.out.println("Produto recebido:" + produto);
+        return produto;
+    }
+
+
+
+}
