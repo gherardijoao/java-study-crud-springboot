1983a49915bf2ed11ffaefd10ed97cc88a7e6438
gherardijoao <gherardijoao@gmail.com>
2025-08-14 23:15:22 -0300

    put, delete and 'search' endpoints added

--- src/main/java/com/example/java_crud/controller/ProdutoController.java
+++ src/main/java/com/example/java_crud/controller/ProdutoController.java
@@ -1,20 +1,52 @@
 package com.example.java_crud.controller;
 import com.example.java_crud.model.Produto;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.java_crud.repository.ProdutoRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+import java.util.UUID;
+import java.util.List;
 
 @RestController
 @RequestMapping ("produtos")
 public class ProdutoController {
 
+    private ProdutoRepository produtoRepository;
+
+    public ProdutoController(ProdutoRepository produtoRepository) {
+        this.produtoRepository = produtoRepository;
+    }
+
     @PostMapping
     public Produto salvar(@RequestBody Produto produto) {
         System.out.println("Produto recebido:" + produto);
+        var id = UUID.randomUUID().toString();
+        produto.setId(id);
+        produtoRepository.save(produto);
         return produto;
     }
 
+    @GetMapping("{id}")
+    public Produto obterPorId(@PathVariable("id") String id) {
+        return produtoRepository.findById(id).orElse(null);
+    }
 
+    @DeleteMapping("{id}")
+    public void deletar(@PathVariable("id") String id){
+        produtoRepository.deleteById(id);
+    }
+
+    @PutMapping("{id}")
+    public void atualizar(@PathVariable("id") String id, @RequestBody Produto produto) {
+        produto.setId(id);
+        produtoRepository.save(produto);
+    }
+
+    @GetMapping
+    public List<Produto> buscar(@RequestParam("nome") String nome){
+       return produtoRepository.findByNome(nome);
+
+
+    }
 
 }
---
+++ src/main/java/com/example/java_crud/repository/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.example.java_crud.repository;
+
+import com.example.java_crud.model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProdutoRepository extends JpaRepository<Produto, String> {
+
+    List<Produto> findByNome(String nome);
+}
